b859e0134e02bee0166d476644f1f74999e53362
4ut0m4t0n <65251886+4ut0m4t0n@users.noreply.github.com>
2020-06-21 20:15:36 -0400

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -1,6 +1,3 @@
-#TO DO: 13 is valid; allow up to length of array
-
-
 #Copyright 2020, 4UT0M4T0N
 #This program is free software: you can redistribute it and/or modify it under the terms of version 3 of the GNU General Public License as published by the Free Software Foundation.  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.  For a copy of the GNU General Public License, see <https://www.gnu.org/licenses/>.
 
